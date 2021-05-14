cbfe03111b6fedcf8e0b57d4ea9f66314f93ea1e
Liam Goring <liam@goring.ca>
2021-05-14 16:43:27 -0500

    CI/CD Feature
    * remove unused scripts

--- deploy/python_deps.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python -m pip install --upgrade pip &&
-pip install --user -r requirements.txt
--- deploy/restart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-./python_deps.sh &&
-ps -A | grep bot.py | awk '{print $1}' | xargs kill -9 $1
