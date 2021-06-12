0dfc422a41c3444e9acc674ad7944dfc76d513eb
Liam Goring <4769068+lagliam@users.noreply.github.com>
2021-06-12 12:48:58 -0500

    removed expletives and updated gitignore and setup script

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@
 mv sample_images images &&
 mv .env.example .env &&
 mkdir guilds
+mv motivashon/motivation1.txt.example motivashon/motivation1.txt &&
+mv motivashon/motivation2.txt.example motivashon/motivation2.txt &&
