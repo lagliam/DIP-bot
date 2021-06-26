49e518a3540f65f21afd474c7b152ccd2532786d
Liam Goring <4769068+lagliam@users.noreply.github.com>
2021-06-25 20:49:19 -0500

    fix to get quote without word wrap

--- bot/motivashon.py
+++ bot/motivashon.py
@@ -14,7 +14,7 @@ async def get_motivated():
 
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype(fm.findfont(fm.FontProperties(family='DejaVu Sans')), 200)
-    text = utility.wrap_by_word(get_quote(), 4)
+    text = get_quote()
     w, h = draw.textsize(text, font)
     drawTextWithOutline(text, img.width/2 - w/2, img.height/2 + h/3, draw, font)
     basewidth = 1024
