267e48b197d3c9694ad75fcdf452e0c6771613cd
Liam Goring <4769068+lagliam@users.noreply.github.com>
2022-01-20 21:29:07 -0600

    fix to image size to keep popping until new file is below limit

--- bot/get_image.py
+++ bot/get_image.py
@@ -1,6 +1,6 @@
 # get_image.py
 # will return one image and mark as viewed
-# returns true if image was sent, falst otherwise
+# returns true if image was sent, false otherwise
 
 from datetime import datetime
 import os
@@ -14,12 +14,13 @@ from bot.utility import image_list, write_viewed_image_list_for_guild
 async def send_image(ctx, seen_images, guild_id, restart=False):
     file_list = image_list()
     filename = file_list.pop(random.randrange(len(file_list)))
-    if os.path.getsize('../images/' + filename) > LIMIT_SIZE:
+    while os.path.getsize('../images/' + filename) > LIMIT_SIZE:
         filename = file_list.pop(random.randrange(len(file_list)))
+        print(f'{datetime.now()}> image {filename} too large to send')
     while filename+'\n' in seen_images:
         try:
             filename = file_list.pop(random.randrange(len(file_list)))
-        except(ValueError):
+        except ValueError:
             filename = ''    
     if not restart and filename:
         await ctx.send('', file=discord.File('../images/' + filename))
