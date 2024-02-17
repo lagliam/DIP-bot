7d6ce4d5da2cbf19626b2dfb7c3dafba486e8b3a
Liam Goring <4769068+lagliam@users.noreply.github.com>
2024-02-16 20:11:39 -0600

    Feature - Rating Reactions
    * Added ability to track and count likes on an image
    * Added getting the top like image and sending to Discord
    * Added database migration to track liked images

---
+++ alembic/versions/ec8fe7b5edff_add_table_to_store_liked_images.py
@@ -0,0 +1,34 @@
+"""Add table to store liked images
+
+Revision ID: ec8fe7b5edff
+Revises: 3044b9ae9b0b
+Create Date: 2024-02-13 00:15:03.043512
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'ec8fe7b5edff'
+down_revision: Union[str, None] = '3044b9ae9b0b'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        'liked_images',
+        sa.Column('id', sa.Integer, autoincrement=True, nullable=False, primary_key=True),
+        sa.Column('filename', sa.String(255), nullable=False),
+        sa.Column('guild_id', sa.BIGINT, nullable=False),
+        sa.Column('channel_id', sa.BIGINT, nullable=False),
+        sa.Column('counter', sa.Integer, nullable=False, default=1),
+        sa.Column('updated', sa.DateTime, nullable=False),
+    )
+
+
+def downgrade():
+    op.drop_table('liked_images')
--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ from discord.ext import tasks
 from dotenv import load_dotenv
 
 from app.app import App
+from app.utilities import database
 from app.utilities.utility import log_event, get_cogs
 
 dotenv_path = Path('.env')
@@ -28,6 +29,33 @@ for cog in command_list:
 @bot.event
 async def on_ready():
     log_event(f'Hello! {bot.user.name} has connected to Discord! 0w0')
+
+
+@bot.event
+async def on_raw_reaction_add(payload):
+    parameters = await extract_parameters(payload)
+    if parameters:
+        database.add_liked_image(parameters)
+
+
+@bot.event
+async def on_raw_reaction_remove(payload):
+    parameters = await extract_parameters(payload)
+    if parameters:
+        database.remove_liked_image(parameters)
+
+
+async def extract_parameters(payload):
+    guild_id = payload.guild_id
+    channel_id = payload.channel_id
+    partial_messageable = bot.get_partial_messageable(channel_id)
+    message_id = payload.message_id
+    message = await partial_messageable.fetch_message(message_id)
+    if message.author.name != bot.user.name:
+        return None
+    filename = message.attachments[0].filename
+    parameters = {'filename': filename, 'guild_id': guild_id, 'channel_id': channel_id}
+    return parameters
 
 
 @tasks.loop(count=1)
